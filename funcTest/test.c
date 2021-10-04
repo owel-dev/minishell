689dd83f938c05b40b53a91cef43ad3900d1a5a5
kyoshong <qkrgytjd0827@naver.com>
2021-10-04 12:53:59 +0900

    function test

---
+++ funcTest/test.c
@@ -0,0 +1,12 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <termcap.h>
+
+int main(int argc, char const *argv[])
+{
+	ioctl()
+	return 0;
+}
