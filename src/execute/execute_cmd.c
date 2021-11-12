055bd622b468bed8f63108f43902557afb60793d
kyoshong <qkrgytjd0827@naver.com>
2021-11-12 16:27:41 +0900

    feat: add redir

--- src/execute/execute_cmd.c
+++ src/execute/execute_cmd.c
@@ -6,19 +6,24 @@
 /*   By: hyospark <hyospark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 16:35:44 by hyospark          #+#    #+#             */
-/*   Updated: 2021/11/07 20:11:42 by hyospark         ###   ########.fr       */
+/*   Updated: 2021/11/12 15:08:59 by hyospark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	pipe_cmd()
+{
+	
+}
 
 int	execute_cmd(t_bundle *bundle)
 {
 	int result;
 
 	result = 0;
-	// if (bundle->is_pipe == TRUE)
-	// 	result = pipe_cmd();
+	if (bundle->is_pipe == TRUE)
+		result = pipe_cmd();
 	// else
 		// 파이프 없는 기본 cmd
 	return (result);
--- src/iohandler/redir_handler.c
+++ src/iohandler/redir_handler.c
@@ -6,13 +6,51 @@
 /*   By: hyospark <hyospark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 13:24:11 by hyospark          #+#    #+#             */
-/*   Updated: 2021/11/12 14:23:22 by hyospark         ###   ########.fr       */
+/*   Updated: 2021/11/12 16:25:38 by hyospark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	redir_handler(t_token *token)
+int	d_redir_cmd(t_bundle *bundle, t_token *token)
+{
+	int	fd;
+
+	fd = set_fd(token);
+	if (fd < 0)
+	{
+		if (token->token_type == D_REDIR_OUT)
+		{
+			fd = open(token->next->content, O_WRONLY | O_APPEND | O_CREAT, 0666);
+		}
+		if (token->token_type == D_REDIR_IN)
+		{
+			while ()
+			{
+				
+			}
+			
+			fd = open(token->next->content, 
+		}
+	}
+	else
+}
+
+int	redir_cmd(t_bundle *s_bundle, t_token *token)
+{
+	
+}
+
+int	set_fd(t_token *token)
+{
+	if (is_fdnum(token->pre->content, 0) == 1 && token->pre->back_space == 0)
+		return (token->pre->content[0] + '0');
+	else if (is_fdnum(token->next->content, 1) == 2 && token->pre->back_space == 0)
+		return (token->pre->content[1] + '0');
+	return (-1);
+}
+
+int	redir_handler(t_bundle *bundle, t_token *token)
 {
 	int	pid;
 	int status;
@@ -22,14 +60,16 @@ int	redir_handler(t_token *token)
 		print_error("FORK_ERROR");
 	if (pid == 0)
 	{
-		if (is_fdnum(token->pre->content, 0) == 1 && token->pre->back_space == 0)
+		
+		if (token->token_type == D_REDIR_IN || token->token_type == D_REDIR_OUT)
 		{
-			dup2(token->pre->content[0] + '0', STDIN_FILENO);
+			d_redir_cmd(bundle, token);
 		}
-		if (is_fdnum(token->next->content, 1) == 2 && token->pre->back_space == 0)
+		else if (token->token_type == REDIR_IN || token->token_type == REDIR_OUT)
 		{
-			dup2(ft_atoi(token->next->content[1] +'0' ), STDOUT_FILENO);
+			redir_cmd(bundle, token);
 		}
+		execute_cmd();
 	}
 	else
 	{
