0708d9104ef24f75560ce50467a24f821a0eeeaa
usanglee <mail@usanglee.com>
2021-11-22 19:19:55 +0900

    Refact: Change to buffer output

--- src/builtin/ft_echo.c
+++ src/builtin/ft_echo.c
@@ -3,34 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyospark <hyospark@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ulee <ulee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 16:38:54 by hyospark          #+#    #+#             */
-/*   Updated: 2021/11/22 01:27:37 by hyospark         ###   ########.fr       */
+/*   Updated: 2021/11/22 19:18:58 by ulee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+char *append_buf(t_bundle *bundle, char *buf)
+{
+	char *ret;
+
+	ret = ft_strjoin(buf, bundle->token->content);
+	if (ret == NULL)
+		return (NULL);
+	if (bundle->token->next && bundle->token->back_space)
+	{
+		ret = ft_strjoin(ret, " ");
+		if (ret == NULL)
+			return (NULL);
+	}
+	return (ret);
+}
+
 int	ft_echo(t_bundle *bundle)
 {
 	int n_option;
+	char *buf_output;
 
 	n_option = 0;
+	buf_output = NULL;
 	while (bundle->token->next && bundle->token->token_type != PIPE) //전체적으로 env 치환 기능 추가
 	{
 		bundle->token = bundle->token->next;
-		if (ft_strcmp(bundle->token->content, "-n") == 0) //strncmp 수정
+		if (ft_strncmp(bundle->token->content, "-n", 2) == 0) //strncmp 수정
 		{
 			n_option = 1;
 			continue ;
 		}
 		if (is_redir_token(bundle->token))
 			redir_handler(bundle);
-		write(1, bundle->token->content, ft_strlen(bundle->token->content));
-		if (bundle->token->next && bundle->token->back_space)
-			write(1, " ", 1);
+		buf_output = append_buf(bundle, buf_output);
+		if (buf_output == NULL)
+			return (FAIL);
 	}
+	write(1, buf_output, ft_strlen(buf_output));
 	if (n_option == 0)
 		write(1, "\n", 1);
 	return (SUCCESS);
