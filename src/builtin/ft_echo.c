2db9bdd64c85f2d0c14fadf69efbfd3ffbf9dd49
Usang Lee <ulee@c8r3s10.42seoul.kr>
2021-12-03 20:37:13 +0900

    Refact: Pass echo tests

--- src/builtin/ft_echo.c
+++ src/builtin/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: ulee <ulee@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 16:38:54 by hyospark          #+#    #+#             */
-/*   Updated: 2021/12/03 20:28:02 by ulee             ###   ########.fr       */
+/*   Updated: 2021/12/03 20:35:25 by ulee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,21 +42,25 @@ int find_another(char *content, char *another)
 int	ft_echo(t_bundle *bundle)
 {
 	int n_option;
+	int word;
 	char *buf_output;
 
 	n_option = 0;
+	word = 0;
 	buf_output = "";
 	while (bundle->token->next && bundle->token->next->token_type != PIPE) //전체적으로 env 치환 기능 추가
 	{
 		bundle->token = bundle->token->next;
 		if (!ft_strncmp(bundle->token->content, "-n", 2)&& \
-			!find_another(bundle->token->content, "-ne"))
+			!find_another(bundle->token->content, "-ne") && \
+			!word)
 		{
 			n_option = 1;
 			continue ;
 		}
 		else
 		{
+			word = 1;
 			buf_output = append_buf(bundle, buf_output);
 			if (buf_output == NULL)
 				return (FAIL);
