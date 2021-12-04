550ab633acf2f121e9f24f9d82ebdfd61037cae6
Usang Lee <ulee@c2r1s7.42seoul.kr>
2021-12-04 19:33:58 +0900

    Fix: ft_cd homepath

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ulee <ulee@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 16:39:16 by hyospark          #+#    #+#             */
-/*   Updated: 2021/12/04 17:57:00 by ulee             ###   ########.fr       */
+/*   Updated: 2021/12/04 19:33:30 by ulee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,22 @@ int  ft_cd(t_bundle *bundle)
 	int		ret_chdir;
 	char	*dir;
 	t_token	*next_token;
+	char 	*home;
 
 	next_token = bundle->token->next;
 	while (bundle->token->next && bundle->token->next->token_type != PIPE)
 	{
 		bundle->token = bundle->token->next;
 	}
-	if (next_token == NULL || is_io_token(next_token) || !ft_strcmp(next_token->content, "~"))
-		dir = ft_getenv(bundle, "HOME");
-	else
-		dir = next_token->content;
+	// if (next_token == NULL || is_io_token(next_token) || !ft_strcmp(next_token->content, "~"))
+	// else
+	dir = next_token->content;
+	if (!ft_strncmp(dir, "~", 1))
+	{
+		home = NULL;
+		home = ft_getenv(bundle, "HOME");
+		dir = ft_strjoin(home, ++dir);
+	}
 	ret_chdir = chdir(dir);
 	if (ret_chdir == -1)
 	{
